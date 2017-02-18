284bd329f32554333f5da95a0ca042b28299bc14
kfpotts1 <kfpotts1@gmail.com>
2017-02-18 12:26:56 -0500

    added distance

---
+++ klml_supervised/distance.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+def innerproduct(x, z=None):
+    """
+    function innerproduct(x,z)
+
+    Computes the inner-product matrix.
+    syntax:
+        d=innerproduct(x,z)
+    Input:
+        x: nxd data matrix with n vectors (rows) of dimensionality d
+        z: mxd data matrix with m vectors (rows) of dimensionality d
+
+    Output:
+        g: Matrix g of size nxm
+            g[i,j] is the inner-product between vectors x[i,:] and z[j,:]
+
+    When called with only one input:
+        innerproduct(x)=innerproduct(x,x)
+    """
+    if z is None:  # case when there is only one input (x)
+        g = np.dot(x, x.transpose())
+    else:  # case when there are two inputs (x,z)
+        z = z.transpose()
+        if x.shape[1] != z.shape[0]:
+            raise ValueError("Invalid Matrices")
+        g = np.dot(x, z)
+    return g
+
+
+def l2distance(x, z=None):
+    """
+    function d=l2distance(x,z)
+
+    Computes the Euclidean distance matrix.
+    syntax:
+        d=l2distance(x,z)
+    Input:
+        x: nxd data matrix with n vectors (rows) of dimensionality d
+        z: mxd data matrix with m vectors (rows) of dimensionality d
+
+    Output:
+        d: Matrix d of size nxm
+            d(i,j) is the Euclidean distance of x(i,:) and z(j,:)
+
+    When called with only one input:
+        l2distance(x)=l2distance(x,x)
+    """
+
+    if z is None:
+        return l2distance(x, x)
+    else:  # case when there are two inputs (x,z)
+        s = (x*x).sum(axis=1).reshape(-1, 1)
+        r = (z*z).sum(axis=1)
+        g = innerproduct(x, z)
+        d_2 = s -2*g + r
+        if x is z:
+            np.fill_diagonal(d_2, 0.0)
+        d_2 = np.sqrt(d_2)
+        return d_2
